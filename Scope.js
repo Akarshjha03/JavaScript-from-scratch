d81eaaa2a3d0d294f6f46a741fbdac6aeedbfdc2
Akarshjha03 <akarshjha.matrix@gmail.com>
2025-07-03 22:37:28 +0530

    Scope

---
+++ Scope.js
@@ -0,0 +1,25 @@
+//scope are of 2 types: global and local
+//global scope is the default scope
+//local scope is created by functions, classes, and modules
+let  a = 300;
+if(true) {
+    let a = 100;
+    const b = 200;
+    console.log("INNER:", a); // 100
+    console.log(b); // 200
+}
+
+console.log("OUTER:", a); // 300
+
+//Nested scopes
+function one(){
+    const username = "Akarsh"
+
+    function two(){
+        const website = "akarsh.me"
+        console.log("Username:", username); // Akarsh
+    }
+    //console.log("Website:", website); // ReferenceError: website is not defined
+    two();
+}
+one(); // Username: Akarsh
