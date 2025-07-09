ffbe4b6e1fff6034a30e5e9d495d346f78f26401
Akarshjha03 <akarshjha.matrix@gmail.com>
2025-07-09 17:32:36 +0530

    While loop

---
+++ whileloop.js
@@ -0,0 +1,13 @@
+let index = 0;
+while(index < 5) {
+    console.log("Current index is: " + index);
+    index++;
+} // The loop will run as long as the condition is true, and it may not run at all if the condition is false initially         
+
+//while loop on arrays
+let fruits = ["apple", "banana", "cherry"];
+let i = 0;
+while(i < fruits.length) {
+    console.log("Current fruit is: " + fruits[i]);
+    i++;
+} // The loop will iterate through the array as long as the condition is true, and it may not run at all if the array is empty
