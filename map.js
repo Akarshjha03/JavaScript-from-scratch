c44a8aeedc64721039b92260a2086f93b9061bba
Akarshjha03 <akarshjha.matrix@gmail.com>
2025-07-09 18:02:18 +0530

    Maps

---
+++ map.js
@@ -0,0 +1,8 @@
+//Maps - This is similar to objects, but the it knows the sequence of the keys
+const map = new Map();
+map.set('IN', 'India');
+map.set('US', 'United States');
+map.set('UK', 'United Kingdom');
+map.set('CA', 'Canada');
+
+console.log(map); // Map(4) { 'IN' => 'India', 'US' => 'United States', 'UK' => 'United Kingdom', 'CA' => 'Canada' }
